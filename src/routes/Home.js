69b83f4fc765cddf2e0b90bec6e59893bddde181
Peter-Roh <rmc2@naver.com>
2021-01-22 16:09:13 +0900

    learning apollo with react

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -47,6 +47,15 @@ const Loading = styled.div`
     margin-top: 10px;
 `;
 
+const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    width: 60%;
+    position: relative;
+    top: -50px;
+`;
+
 function Home() {
     const { loading, data } = useQuery(GET_MOVIES);
 
@@ -57,8 +66,15 @@ function Home() {
                 <Subtitle>I love GraphQL!</Subtitle>
             </Header>
             {loading && <Loading>Loading...</Loading>}
-            {!loading && data.movies && data.movies.map(m => 
-                <Movie key={m.id} id={m.id} />)}
+            {!loading && data.movies && (
+                <Movies>
+                    {
+                        data.movies.map(m => 
+                            <Movie key={m.id} id={m.id} isLiked={m.isLiked} bg={m.medium_cover_image} />
+                        )
+                    }
+                </Movies>
+            )}
         </Container>
     );
 };
